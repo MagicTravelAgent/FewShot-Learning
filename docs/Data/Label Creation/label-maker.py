d941183e7ac856d7e668e7f7500e3aa57151853a
Lucas Puddifoot <lucas_puddifoot@hotmail.co.uk>
2022-12-02 14:16:44 +0100

    Formatting

--- docs/Data/Label Creation/label-maker.py
+++ docs/Data/Label Creation/label-maker.py
@@ -9,50 +9,47 @@ annotation_loc = "C:/Users/LPUDDIFO/OneDrive - Capgemini/Documents/Internship/Re
 # Get all the file names that i need to use
 name_list = glob.glob(annotation_loc+'*.png')
 # taking the colour images of the annotations
-drop_mask = np.flatnonzero(np.core.defchararray.find(name_list,"color")!=-1)
-name_list = np.take(name_list, drop_mask) 
+drop_mask = np.flatnonzero(np.core.defchararray.find(name_list, "color") != -1)
+name_list = np.take(name_list, drop_mask)
 
 # conversion dict
 name_dict = {}
-support = [] # not all images have people and they should be excluded from the support set
+support = []  # not all images have people and they should be excluded from the support set
 
 # open each file in turn
 for index, image_name in tqdm(enumerate(name_list)):
 
     # open the image to select the people
     im = Image.open(image_name)
-    pix = np.array(im)[:,:,:3]
+    pix = np.array(im)[:, :, :3]
 
-    for i in range(pix.shape[0]): # for every pixel:
+    for i in range(pix.shape[0]):  # for every pixel:
         for j in range(pix.shape[1]):
-            comp = tuple(pix[i,j])
+            comp = tuple(pix[i, j])
             if comp == (220, 20, 60):
-                # turn the people white 
-                pix[i,j] = [255, 255, 255]
-                
+                # turn the people white
+                pix[i, j] = [255, 255, 255]
+
             else:
                 # turn the background black
-                pix[i,j] = [0, 0, 0]
+                pix[i, j] = [0, 0, 0]
 
-    im = Image.fromarray(pix) # save the new blakc and white image
-
+    im = Image.fromarray(pix)
+    im = im.convert("1")  # save the new black and white image
     # save the masks in a new location
-    im=im.convert("1")
     im.save(annotation_loc+"persons/"+str(index)+'.png')
 
     # identify the name of the image
-    # print(image_name)
     name = image_name.split('\\')[-1]
-    # print(name)
 
     # make a note of the name changes in a dictionary
     name_dict[name[:-4]] = index
-    if(np.sum(pix) > 1):
+    if(np.sum(pix) > 1):  # make a note of which images had people in them
         support.append(index)
 
 # save the name changes and the valid support queries
 with open('docs/Data/Label Creation/name_changes.txt', 'w') as convert_file:
-     convert_file.write(json.dumps(name_dict))
+    convert_file.write(json.dumps(name_dict))
 
 with open('docs/Data/Label Creation/support_list.txt', 'w') as convert_file:
-     convert_file.write('\n'.join(str(line) for line in support))
+    convert_file.write('\n'.join(str(line) for line in support))
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import numpy as np
 
 from docs.HSNet.Model.HSNet import HypercorrSqueezeNetwork
-from docs.HSNet.Common import  Utils
+from docs.HSNet.Common import Utils
 from docs.HSNet.Common.Visualizer import Visualizer
 from docs.HSNet.Common.Evaluator import Evaluator
 from docs.HSNet.DataLoader.FSSDataset import FSSDataset
@@ -19,7 +19,6 @@ def test(model, dataloader, nshot):
     ious = []
     for idx, batch in enumerate(dataloader):
 
-
         # 1. Hypercorrelation Squeeze Networks forward pass
         batch = Utils.to_cuda(batch)
         pred_mask = model.module.predict_mask_nshot(batch, nshot=nshot)
@@ -27,7 +26,8 @@ def test(model, dataloader, nshot):
         assert pred_mask.size() == batch['query_mask'].size()
 
         # 2. Evaluate prediction
-        area_inter, area_union = Evaluator.classify_prediction(pred_mask.clone(), batch)
+        area_inter, area_union = Evaluator.classify_prediction(
+            pred_mask.clone(), batch)
         # print("IOU:",area_inter/area_union)
         iou = area_inter[1].float() / area_union[1].float()
 
@@ -35,32 +35,33 @@ def test(model, dataloader, nshot):
         if Visualizer.visualize:
             Visualizer.visualize_prediction_batch(batch['support_imgs'], batch['support_masks'],
                                                   batch['query_img'], batch['query_mask'],
-                                                  pred_mask, idx, iou_b = iou)
+                                                  pred_mask, idx, iou_b=iou)
         ious.append(iou[0].float().item())
     return np.array(ious).mean()
 
 
 if __name__ == '__main__':
 
-    #===============================================================================
+    # ===============================================================================
     # Let's a-go!
-    #===============================================================================
+    # ===============================================================================
     # Arguments parsing
     args = {
-      "datapath": 'docs/Data/',
-      "benchmark": 'custom', # dataloader selection
-      "bsz": 1,
-      "lr": 1e-3,
-      "nworker": 0,
-      "load": "docs/HSNet/Model/res101_pas/res101_pas_fold3/best_model.pt",
-      "nshot": 1,
-      "backbone": 'resnet101', # choices=['vgg16', 'resnet50', 'resnet101']
-      "visualize": True,
-      "use_original_imgsize": False
+        "datapath": 'docs/Data/',
+        "benchmark": 'custom',  # dataloader selection
+        "bsz": 1,
+        "lr": 1e-3,
+        "nworker": 0,
+        "load": "docs/HSNet/Model/res101_pas/res101_pas_fold3/best_model.pt",
+        "nshot": 1,
+        "backbone": 'resnet101',  # choices=['vgg16', 'resnet50', 'resnet101']
+        "visualize": True,
+        "use_original_imgsize": False
     }
 
     # Model initialization
-    model = HypercorrSqueezeNetwork(args["backbone"], args["use_original_imgsize"])
+    model = HypercorrSqueezeNetwork(
+        args["backbone"], args["use_original_imgsize"])
     model.eval()
 
     # Device setup
@@ -70,7 +71,8 @@ if __name__ == '__main__':
     model.to(device)
 
     # Load trained model
-    if args["load"] == '': raise Exception('Pretrained model not specified.')
+    if args["load"] == '':
+        raise Exception('Pretrained model not specified.')
     model.load_state_dict(torch.load(args["load"]))
 
     # Helper classes (for testing) initialization
@@ -78,8 +80,10 @@ if __name__ == '__main__':
     Visualizer.initialize(args["visualize"])
 
     # Dataset initialization
-    FSSDataset.initialize(img_size=400, datapath=args["datapath"], use_original_imgsize=args["use_original_imgsize"])
-    dataloader_test = FSSDataset.build_dataloader(benchmark = args["benchmark"], experiment = 'test', shot = args["nshot"])
+    FSSDataset.initialize(
+        img_size=400, datapath=args["datapath"], use_original_imgsize=args["use_original_imgsize"])
+    dataloader_test = FSSDataset.build_dataloader(
+        benchmark=args["benchmark"], experiment='test', shot=args["nshot"])
 
     # Test HSNet
     with torch.no_grad():
